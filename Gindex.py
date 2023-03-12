61bd6950cfc736cab14b6c425ce00ef8e461f58d
Akalikaton <huaizhi1998@outlook.com>
2023-03-12 17:28:32 +0100

    UA title change

--- Gindex.py
+++ Gindex.py
@@ -7,7 +7,7 @@ data_folder = './htmldata'
 file_names = os.listdir(data_folder)
 
 # 生成HTML代码
-html = '<!DOCTYPE html>\n<html>\n  <head>\n    <title>My Data Files</title>\n  </head>\n  <body>\n    <h1>My Data Files</h1>\n    <ul>\n'
+html = '<!DOCTYPE html>\n<html>\n  <head>\n    <title>UA Deutsch Lernen</title>\n  </head>\n  <body>\n    <h1>UA Deutsch Lernen</h1>\n    <ul>\n'
 for file_name in file_names:
     html += f'      <li><a href="{data_folder}/{file_name}">{file_name}</a></li>\n'
 html += '    </ul>\n  </body>\n</html>'
